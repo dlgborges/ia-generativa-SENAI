3ec8f9c08738bb70c465b6423316362edf1d4861
Dashiell <dlgborges@outlook.com>
2026-06-09 22:02:22 -0300

    class_2 more pro programming

---
+++ Classes/class_2/exercises.py
@@ -0,0 +1,34 @@
+#calculadora
+
+import streamlit as st
+st.title('Calculadora')
+
+number_1 = st.number_input('first number: ')
+number_2 = st.number_input('second number: ')
+
+if st.button('RESULTADO SOMA'):
+    soma = number_1 + number_2
+    st.success(soma)
+
+#calculadora imc
+
+import streamlit as st
+st.title('Calculadora de IMC')
+
+weight = st.number_input('weight: ')
+height = st.number_input('height: ')
+
+if st.button('RESULTADO IMC'):
+    imc = weight / (height ** 2)
+    st.success(imc)
+
+#calculadora imc
+
+import streamlit as st
+st.title('Conversor de temperatura')
+
+celsius = st.number_input('Degrees in Celsius: ')
+
+if st.button('Convert Celsius to Fahrenheit'):
+    celsius_fahrenheit = (celsius*1.8)+32
+    st.success(celsius_fahrenheit)
